0f6595e9775b2b59fae5dc79747fca065754470c
sniemann <sascha.niemann66@gmx.de>
2025-06-25 18:23:42 +0200

    docker container: restart unless stopped

--- deployment/start_container.sh
+++ deployment/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CURRENT_DIR="$(pwd)"
-docker run --restart=always -v "${CURRENT_DIR}/data:/app/data" -p 8501:8501 --name festival-finder-container --rm -d --env-file ./.env festival-finder:latest
+docker run --restart=unless-stopped -v "${CURRENT_DIR}/data:/app/data" -p 8501:8501 --name festival-finder-container -d --env-file ./.env festival-finder:latest
